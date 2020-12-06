bfe307119b52ea4aac880b6e9be2fc29f321d026
Bartosz Gościński <bargosc@gmail.com>
2020-12-06 21:20:45 +0100

    it works!

--- lib/sendmail/sendmail.h
+++ lib/sendmail/sendmail.h
@@ -5,13 +5,115 @@
 // Config must be included first
 
 #include <TinyGsmClient.h>
+#include <b64.h>
 #include <secrets.h>
 
 #define EXPECT(x, ret) \
   if (!(x))            \
     return (ret);
 
-bool sendTextMail(TinyGsm& modem, String& message) {
+inline int16_t streamGetIntBefore(TinyGsm& modem, char lastChar) {
+  char buf[7];
+  size_t bytesRead =
+      modem.stream.readBytesUntil(lastChar, buf, static_cast<size_t>(7));
+  // if we read 7 or more bytes, it's an overflow
+  if (bytesRead && bytesRead < 7) {
+    buf[bytesRead] = '\0';
+    int16_t res = atoi(buf);
+    return res;
+  }
+
+  return -9999;
+}
+
+#define ENC_SIZE(size) (((size) + 2) / 3) << 2
+#define DEC_SIZE(size) (((size) >> 2) * 3)
+#define BUF_SIZE (size_t)3 * 512
+#define ENC_BUF_SIZE ENC_SIZE(BUF_SIZE)
+unsigned char buf[BUF_SIZE];
+unsigned char encBuf[ENC_BUF_SIZE];
+
+void printProgress(int len, int pos) {
+  String pct = len <= 0 ? "?" : String((100 * pos) / len);
+  SerialMon.print("Transferring photo... ");
+  SerialMon.print(pct);
+  SerialMon.print("% (");
+  SerialMon.print(pos);
+  SerialMon.print(" / ");
+  SerialMon.print(len);
+  SerialMon.println(" B)");
+}
+
+bool seekAtt(HttpClient* att, const String& path, int seekBytes, int* len) {
+  att->get(path);
+  att->responseStatusCode();
+
+  // don't use att->contentLength() because it's case sensitive
+  while (!att->endOfHeadersReached() && att->headerAvailable()) {
+    auto name = att->readHeaderName();
+    if (name.equalsIgnoreCase("content-length")) {
+      *len = att->readHeaderValue().toInt();
+      att->skipResponseHeaders();
+    }
+  }
+
+  auto lastRead = millis();
+  seekBytes = min(seekBytes, *len);
+
+  SerialMon.print("Attempting to seek through ");
+  SerialMon.print(seekBytes);
+  SerialMon.print(" of ");
+  SerialMon.print(*len);
+  SerialMon.println(" bytes");
+
+  int c;
+  for (int i = 0; i < seekBytes; i++) {
+    sendHeartBeatIfNecessary();
+    while ((c = att->read()) == -1) {
+      delay(100);
+      if ((millis() - lastRead) > 3000) {
+        return false;
+      }
+    }
+    if (i < 100 || (seekBytes - i) < 100) {
+      SerialMon.printf("%02x ", c);
+    } else if (i == 100 && (seekBytes - i) > 100) {
+      SerialMon.print(".. ");
+    }
+    lastRead = millis();
+  }
+  SerialMon.println();
+
+  return true;
+}
+
+/**
+ * value guaranteed to be 3*n
+ */
+size_t getToRead(TinyGsm& modem) {
+  EXPECT(modem.waitResponse(3 * 60 * 1000, "+SMTPFT: 1,") == 1, false)
+  size_t maxSize = streamGetIntBefore(modem, '\n');
+  size_t maxEncSize = maxSize - (maxSize % 4);
+  return min(BUF_SIZE, DEC_SIZE(maxEncSize));
+}
+
+void awaitCamData(HttpClient* att, const String& path, int pos, int* len) {
+  auto waitForCamDataStart = millis();
+  do {
+    delay(50);
+    sendHeartBeatIfNecessary();
+    if ((millis() - waitForCamDataStart) > 1000) {
+      SerialMon.println("Lost connection. Resurrecting it...");
+      seekAtt(att, path, pos, len);
+      waitForCamDataStart = millis();
+    }
+  } while (pos < *len && !att->available());
+}
+
+bool sendMail(TinyGsm& modem,
+              const String& message,
+              HttpClient* att,
+              const String& path) {
   modem.sendAT(GF("+EMAILCID=1"));
   EXPECT(modem.waitResponse() == 1, false)
 
@@ -34,18 +136,75 @@ bool sendTextMail(TinyGsm& modem, String& message) {
       GF("+SMTPRCPT=0,0,\"" S_EMAIL_RCPT "\",\"" S_EMAIL_RCPT_NAME "\""));
   EXPECT(modem.waitResponse() == 1, false)
 
-  modem.sendAT(GF("+SMTPSUB=\"test\""));
+  modem.sendAT(GF("+SMTPSUB=\"Fotka\""));
   EXPECT(modem.waitResponse() == 1, false)
 
-  modem.sendAT("+SMTPBODY=", String(message.length(), 10));
+  modem.sendAT(GF("+SMTPBODY="), String(message.length(), 10));
   EXPECT(modem.waitResponse("DOWNLOAD") == 1, false)
 
   modem.streamWrite(message, GSM_NL);
   modem.stream.flush();
   EXPECT(modem.waitResponse() == 1, false)
 
+  if (att) {
+    String name = path.substring(path.lastIndexOf('/') + 1);
+    modem.sendAT(GF("+SMTPFILE=2,\""), name, GF("\",1"));
+    EXPECT(modem.waitResponse() == 1, false)
+  }
+
   modem.sendAT(GF("+SMTPSEND"));
   EXPECT(modem.waitResponse() == 1, false)
 
+  if (att) {
+    int pos = 0;
+    int len = -1;
+
+    // get len, start the connection
+    seekAtt(att, path, 0, &len);
+
+    while (1) {
+      if (pos == len) {
+        SerialMon.println("Transfer complete");
+        break;
+      }
+
+      printProgress(len, pos);
+      awaitCamData(att, path, pos, &len);
+
+      auto toRead = getToRead(modem);
+      auto readBytes = att->readBytes(buf, toRead);
+      pos += readBytes;
+
+      while (readBytes % 3 && readBytes < toRead && pos < len) {
+        SerialMon.print("Aligning data to encode to be a multiply of 3... ");
+        awaitCamData(att, path, pos, &len);
+        if (pos < len) {
+          SerialMon.println("+1 B");
+          buf[readBytes++] = att->read();
+          pos++;
+        } else {
+          SerialMon.println("Reached EOF");
+          break;
+        }
+      }
+
+      auto encSize = b64_encode(buf, readBytes, encBuf, ENC_BUF_SIZE);
+      modem.sendAT(GF("+SMTPFT="), String(encSize));
+      EXPECT(modem.waitResponse("+SMTPFT: 2,") == 1, false)
+      EXPECT(modem.waitResponse(String(encSize).c_str()) == 1, false)
+
+      modem.stream.write(encBuf, encSize);
+      modem.stream.flush();
+
+      EXPECT(modem.waitResponse(3 * 60 * 1000) == 1, false)
+    }
+
+    EXPECT(modem.waitResponse("+SMTPFT: 1,") == 1, false)
+    streamGetIntBefore(modem, '\n');
+    modem.sendAT(GF("+SMTPFT=0"));
+    EXPECT(modem.waitResponse() == 1, false)
+  }
+
+  SerialMon.println("Mail sent");
   return true;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,38 @@
 #include "config.h"
 // Config must be included first
 
+// Set serial for debug console (to Serial Monitor, default speed 115200)
+#define SerialMon Serial
+// Set serial for AT commands (to SIM800 module)
+#define SerialAT Serial1
+
+#define TINY_GSM_USE_GPRS true
+#define TINY_GSM_USE_WIFI false
+// #define TINY_GSM_DEBUG SerialMon
+
+void sendHeartBeatIfNecessary();
+#define TINY_GSM_YIELD()        \
+  {                             \
+    sendHeartBeatIfNecessary(); \
+    delay(1);                   \
+  }
+
 #include <Arduino.h>
-#include <HTTPClient.h>
+#include <ArduinoHttpClient.h>
 #include <TinyGsmClient.h>
 #include <WiFi.h>
 #include <Wire.h>
 #include <secrets.h>
 #include <sendmail.h>
 
-WiFiClient hbClient;
-
-// Set serial for debug console (to Serial Monitor, default speed 115200)
-#define SerialMon Serial
-
-// Set serial for AT commands (to SIM800 module)
-#define SerialAT Serial1
-
+// #define DUMP_AT_COMMANDS
+#ifdef DUMP_AT_COMMANDS
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, SerialMon);
+TinyGsm modem(debugger);
+#else
 TinyGsm modem(SerialAT);
+#endif
 
 bool setPowerBoostKeepOn(int en) {
   Wire.beginTransmission(IP5306_ADDR);
@@ -31,63 +46,20 @@ bool setPowerBoostKeepOn(int en) {
   return Wire.endTransmission() == 0;
 }
 
-void setup() {
-  // Set console baud rate
-  SerialMon.begin(115200);
-
-  // Keep power when running from battery
-  Wire.begin(I2C_SDA, I2C_SCL);
-  bool isOk = setPowerBoostKeepOn(1);
-  SerialMon.println(String("IP5306 KeepOn ") + (isOk ? "OK" : "FAIL"));
-
-  // Set modem reset, enable, power pins
-  pinMode(MODEM_PWKEY, OUTPUT);
-  pinMode(MODEM_RST, OUTPUT);
-  pinMode(MODEM_POWER_ON, OUTPUT);
-  digitalWrite(MODEM_PWKEY, LOW);
-  digitalWrite(MODEM_RST, HIGH);
-  digitalWrite(MODEM_POWER_ON, HIGH);
-
-  // Set GSM module baud rate and UART pins
-  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
-  delay(3000);
-
-  // Restart SIM800 module, it takes quite some time
-  // To skip it, call init() instead of restart()
-  // SerialMon.println("Initializing modem...");
-  // modem.factoryDefault();
-  // modem.restart();
-  // modem.waitForNetwork();
-  // modem.getGsmLocation();
-  // modem.gprsConnect("virgin-internet");
-
-  // auto msg = String("this is a message\nsecond line");
-  // sendTextMail(modem, msg);
+const String doGet(HttpClient* http, const String& path) {
+  SerialMon.print("GET ");
+  SerialMon.print(path);
+  SerialMon.print(" [");
+  SerialMon.print(http->get(path));
+  SerialMon.print(" ");
+  SerialMon.print(http->responseStatusCode());
+  SerialMon.println("]");
+  return http->responseBody();
 }
 
-String get(const char* url) {
-  SerialMon.println(url);
-  HTTPClient http;
-  http.begin(url);
-  http.GET();
-  auto res = http.getString();
-  SerialMon.println(res);
-  http.end();
-  return res;
-}
-
+WiFiClient hbClient;
 unsigned long lastHB = 0;
-void loop() {
-  if (!WiFi.isConnected()) {
-    SerialMon.print("Setting up WiFi connection");
-    WiFi.begin(S_CAM_SSID, S_CAM_PASS);
-    while (!WiFi.isConnected()) {
-      SerialMon.print('.');
-      delay(500);
-    }
-    SerialMon.println("\nWiFi connection established");
-  }
-
+void sendHeartBeatIfNecessary() {
   if (!hbClient.connected()) {
     SerialMon.print("Setting up cam's HeartBeat connection");
     hbClient.connect(S_CAM_IP, 3333);
@@ -98,41 +70,158 @@ void loop() {
     SerialMon.println("\nCam's HeartBeat connection established");
   }
 
-  if (millis() - lastHB > 5000) {
-    SerialMon.println("Sending HeartBeat signal");
+  if (millis() - lastHB > 4000) {
     lastHB = millis();
     hbClient.write("02:001:0");
     hbClient.flush();
   }
+}
 
-  get("http://" S_CAM_IP ":" C_YI_CONNECT);
+void skipAtStreamUntil(char c) {
+  while (1) {
+    if (SerialAT.read() == c) {
+      return;
+    }
+    delay(10);
+    sendHeartBeatIfNecessary();
+  }
+}
 
-  get("http://" S_CAM_IP ":" C_YI_VIDEO_RECORD_OFF);
+const String getMailBody() {
+  modem.sendAT(GF("+CSCS=\"GSM\""));
+  if (modem.waitResponse() != 1) {
+    skipAtStreamUntil('\n');
+    return "";
+  }
+  modem.sendAT(GF("+CUSD=1,\"*101#\",15"));
+  if (modem.waitResponse(30000, "+CUSD: 0, \"") != 1) {
+    skipAtStreamUntil('\n');
+    return "";
+  }
 
-  get("http://" S_CAM_IP ":" C_YI_MODE_VIDEO);
+  auto body = modem.stream.readStringUntil('"');
+  SerialMon.println(body);
+  skipAtStreamUntil('\n');
+  return body;
+}
 
-  get("http://" S_CAM_IP ":" C_YI_VIDEO_RECORD_OFF);
+void setup() {
+  SerialMon.begin(115200);
 
-  get("http://" S_CAM_IP ":" C_YI_VIDEO_AUTO_START_OFF);
+  // Keep power when running from battery
+  Wire.begin(I2C_SDA, I2C_SCL);
+  setPowerBoostKeepOn(1);
 
-  get("http://" S_CAM_IP ":" C_YI_PHOTO_RESOLUTION1920);
+  // Set modem reset, enable, power pins
+  pinMode(MODEM_PWKEY, OUTPUT);
+  pinMode(MODEM_RST, OUTPUT);
+  pinMode(MODEM_POWER_ON, OUTPUT);
+  digitalWrite(MODEM_PWKEY, LOW);
+  digitalWrite(MODEM_RST, HIGH);
+  digitalWrite(MODEM_POWER_ON, HIGH);
 
-  delay(100);
+  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  delay(6000);
 
-  get("http://" S_CAM_IP ":" C_YI_MODE_PHOTO);
+  if (!WiFi.isConnected()) {
+    SerialMon.print("Setting up WiFi connection");
+    WiFi.begin(S_CAM_SSID, S_CAM_PASS);
+    while (!WiFi.isConnected()) {
+      SerialMon.print('.');
+      delay(500);
+    }
+    SerialMon.println("\nWiFi connection established");
+  }
 
-  auto res = get("http://" S_CAM_IP ":" C_YI_TAKE_PHOTO);
-  SerialMon.println(res);
+  sendHeartBeatIfNecessary();
+
+  WiFiClient camClient;
+  if (!camClient.connected()) {
+    SerialMon.print("Setting up cam's API connection");
+    camClient.connect(S_CAM_IP, 80);
+    while (!camClient.connected()) {
+      SerialMon.print('.');
+      delay(500);
+    }
+    SerialMon.println("\nCam's API connection established");
+  }
+
+  HttpClient camHttp(camClient, S_CAM_IP);
+  camHttp.connectionKeepAlive();
+
+  doGet(&camHttp, "/?custom=1&" C_YI_CONNECT);
+  sendHeartBeatIfNecessary();
+
+  doGet(&camHttp, "/?custom=1&" C_YI_VIDEO_RECORD_OFF);
+  sendHeartBeatIfNecessary();
+
+  doGet(&camHttp, "/?custom=1&" C_YI_MODE_VIDEO);
+  sendHeartBeatIfNecessary();
+
+  doGet(&camHttp, "/?custom=1&" C_YI_VIDEO_RECORD_OFF);
+  sendHeartBeatIfNecessary();
+
+  doGet(&camHttp, "/?custom=1&" C_YI_VIDEO_AUTO_START_OFF);
+  sendHeartBeatIfNecessary();
+
+  doGet(&camHttp, "/?custom=1&" C_YI_PHOTO_RESOLUTION1280);
+
+  do {
+    sendHeartBeatIfNecessary();
+    SerialMon.println("Restarting modem... ");
+    modem.restart();
+    sendHeartBeatIfNecessary();
+    modem.waitForNetwork();
+    sendHeartBeatIfNecessary();
+
+    SerialMon.println("Initializing GPRS... ");
+  } while (!modem.gprsConnect("virgin-internet"));
+  sendHeartBeatIfNecessary();
+
+  int year = 0;
+  int month = 0;
+  int day = 0;
+  int hour = 0;
+  int minute = 0;
+  int second = 0;
+  if (modem.getNetworkTime(&year, &month, &day, &hour, &minute, &second,
+                           nullptr)) {
+    sendHeartBeatIfNecessary();
+    char timeBuf[30];
+    //                     yyyy- mm - dd _ HH : mm :  ss
+    sprintf(timeBuf, "&str=%04d-%02d-%02d_%02d:%02d:%02d", year, month, day,
+            hour, minute, second);
+    String timeCmd = "/?custom=1&" C_YI_CLOCK;
+    doGet(&camHttp, timeCmd + timeBuf);
+    sendHeartBeatIfNecessary();
+  }
+
+  doGet(&camHttp, "/?custom=1&" C_YI_MODE_PHOTO);
+  sendHeartBeatIfNecessary();
+
+  auto res = doGet(&camHttp, "/?custom=1&" C_YI_TAKE_PHOTO);
+  sendHeartBeatIfNecessary();
   auto start = res.indexOf("<FPATH>A:") + 9;
   auto end = res.indexOf("</FPATH", start);
+  if (start == -1 || end == -1) {
+    SerialMon.println("Couldn't find path in response:");
+    SerialMon.println(res);
+    return;
+  }
+
   auto path = res.substring(start, end);
   path.replace('\\', '/');
-  SerialMon.println(path);
 
-  while (SerialAT.available()) {
-    SerialMon.write(SerialAT.read());
-  }
-  while (SerialMon.available()) {
-    SerialAT.write(SerialMon.read());
-  }
+  doGet(&camHttp, "/?custom=1&" C_YI_MODE_FILE);
+  sendHeartBeatIfNecessary();
+
+  auto mailBody = getMailBody();
+  sendHeartBeatIfNecessary();
+  sendMail(modem, mailBody, &camHttp, path);
+
+  camHttp.stop();
+}
+
+void loop() {
+  delay(100);
 }
